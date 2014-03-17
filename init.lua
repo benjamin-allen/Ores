71035b86c520a930bdc87f3a1b54446a126e0e95
NANOsoldierEnke <nanosoldierenke@gmail.com>
2014-03-16 20:35:59 -0500

    Ores 0.a1 Alabaster added

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+--Adamantine Ore
+
 minetest.register_node("ores:adamantineore", {
 	description = "Adamantine Ore",
 	tiles = {"default_stone.png^ores_adamantineore.png"},
@@ -11,14 +13,41 @@ minetest.register_ore({
 	ore_type		= "scatter",
 	ore				= "ores:adamantineore",
 	wherein			= "default:stone",
-	clust_scarcity	= 3*3*3,
-	clust_num_ores	= 2,
+	clust_scarcity	= 30*30*30,
+	clust_num_ores	= 5,
 	clust_size		= 3,
 	height_min		= -31000,
-	height_max		= 64,
+	height_max		= -10000,
 })
 
 minetest.register_craftitem("ores:adamantinelump", {
 	description = "Adamantine Lump",
 	inventory_image = "ores_adamantinelump.png"
+})
+
+--Alabaster
+
+minetest.register_node("ores:alabasterore", {
+	description = "Alabaster Ore",
+	tiles = {"default_stone.png^ores_alabasterore.png"},
+	is_ground_content = true,
+	groups = {cracky=3},
+	drop = 'ores:alabasterlump',
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_ore({
+	ore_type		= "scatter",
+	ore				= "ores:alabasterore",
+	wherein			= "default:stone",
+	clust_scarcity	= 13*13*13,
+	clust_num_ores	= 10,
+	clust_size		= 4,
+	height_min		= -256,
+	height_max		= 256,
+})
+
+minetest.register_craftitem("ores:alabasterlump", {
+	description = "Alabaster Lump",
+	inventory_image = "ores_alabasterlump.png"
 })
